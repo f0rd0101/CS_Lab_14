4db96a360ee07dc294e7401c3ca02496ce717ff2
Nick <melancholic8078@gmail.com>
2025-05-17 01:24:15 +0300

    done

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void capitalizeWords(char *str) {
+    int newWord = 1;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isspace(str[i])) {
+            newWord = 1; 
+        } else if (newWord && isalpha(str[i])) {
+            str[i] = toupper(str[i]);
+            newWord = 0; 
+        }
+    }
+}
+
+int main() {
+    char str[1000];
+
+    printf("Введи рядок: ");
+    fgets(str, sizeof(str), stdin);
+
+    capitalizeWords(str);
+
+    printf("Результат: %s", str);
+
+    return 0;
+}
